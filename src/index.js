768593d84248ee18550fe22ad58d708bf62d065b
Anand Nakat <nakatanand2000@gmail.com>
2021-01-14 18:49:57 +0530

    Testing Deployment on Netlify

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./tailwind.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
@@ -22,8 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
